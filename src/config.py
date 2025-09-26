fb8c212360c99454d8a791ba18b979b133a1c0a7
XauddeR <vic.aedo20@gmail.com>
2025-09-25 21:26:24 -0300

    Cambios por seguridad

--- src/config.py
+++ src/config.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Config:
-    SECRET_KEY = os.getenv('SECRET_KEY', 'root351')
+    SECRET_KEY = os.getenv('SECRET_KEY')
 
     OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
     DEEPL_API_KEY = os.getenv('DEEPL_API_KEY')
