6bc0e1f745aa306d20b1a1efa69dfe20e2383d8b
XauddeR <vic.aedo20@gmail.com>
2025-11-25 22:27:54 -0300

    Get .env variables

--- src/config.py
+++ src/config.py
@@ -5,13 +5,25 @@ load_dotenv()
 
 class Config:
     SECRET_KEY = os.getenv('SECRET_KEY')
+    SECURITY_PASSWORD_SALT = os.getenv('SECURITY_PASSWORD_SALT')
 
+    # API Keys
     OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
     DEEPL_API_KEY = os.getenv('DEEPL_API_KEY')
     ASSEMBLYAI_API_KEY = os.getenv('ASSEMBLYAI_API_KEY')
 
+    # Database info
     MYSQL_HOST = os.environ.get('MYSQL_HOST')
     MYSQL_USER = os.environ.get('MYSQL_USER')
     MYSQL_PASSWORD = os.environ.get('MYSQL_PASSWORD')
     MYSQL_DB = os.environ.get('MYSQL_DB')
     MYSQL_CURSORCLASS = 'DictCursor'
+
+    # Google SMTP config
+    MAIL_SERVER = os.getenv('MAIL_SERVER')
+    MAIL_PORT = int(os.getenv('MAIL_PORT'))
+    MAIL_USERNAME = os.getenv('MAIL_USERNAME')
+    MAIL_PASSWORD = os.getenv('MAIL_PASSWORD')
+    MAIL_USE_TLS = os.getenv('MAIL_USE_TLS')
+    MAIL_USE_SSL = os.getenv('MAIL_USE_SSL')
+    MAIL_DEFAULT_SENDER = os.getenv("MAIL_DEFAULT_SENDER")
