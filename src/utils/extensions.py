4bfe6615834243f429ff16792ed980053818cedb
XauddeR <vic.aedo20@gmail.com>
2025-11-27 17:13:36 -0300

    extensions

--- src/utils/extensions.py
+++ src/utils/extensions.py
@@ -5,8 +5,8 @@ from flask_socketio import SocketIO
 
 mysql = MySQL()
 socketio = SocketIO(
-    cors_allowed_origins="*",
-    async_mode="threading"
+    cors_allowed_origins = '*',
+    async_mode = 'threading'
 )
 mail = Mail()
 login_manager = LoginManager()
