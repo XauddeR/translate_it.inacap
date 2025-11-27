9fd0fb255c3c2b34e98e9302495be1153dfa3ba7
XauddeR <vic.aedo20@gmail.com>
2025-11-27 17:10:37 -0300

    App configuration fix

--- src/config.py
+++ src/config.py
@@ -24,6 +24,6 @@ class Config:
     MAIL_PORT = int(os.getenv('MAIL_PORT'))
     MAIL_USERNAME = os.getenv('MAIL_USERNAME')
     MAIL_PASSWORD = os.getenv('MAIL_PASSWORD')
-    MAIL_USE_TLS = os.getenv('MAIL_USE_TLS')
-    MAIL_USE_SSL = os.getenv('MAIL_USE_SSL')
+    MAIL_USE_TLS = os.getenv('MAIL_USE_TLS') == 'True'
+    MAIL_USE_SSL = os.getenv('MAIL_USE_SSL') == 'True'
     MAIL_DEFAULT_SENDER = os.getenv("MAIL_DEFAULT_SENDER")
